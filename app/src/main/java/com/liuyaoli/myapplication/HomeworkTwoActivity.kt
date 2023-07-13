78852f059717222a476bcb839f7f0fa6b27e895e
211250165 <211250165@smail.nju.edu.cn>
2023-07-13 16:56:56 +0800

    设计作业要求的布局

---
+++ app/src/main/java/com/liuyaoli/myapplication/HomeworkTwoActivity.kt
@@ -0,0 +1,11 @@
+package com.liuyaoli.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeworkTwoActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.homework_two)
+    }
+}
