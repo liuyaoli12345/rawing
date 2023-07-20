53aebfc8804b2b55cfbd06962121c561d3a3dc51
211250165 <211250165@smail.nju.edu.cn>
2023-07-20 17:44:31 +0800

    实现用户向下滑动时切换视频

--- app/src/main/java/com/liuyaoli/myapplication/VideoStreamActivity.kt
+++ app/src/main/java/com/liuyaoli/myapplication/VideoStreamActivity.kt
@@ -3,6 +3,8 @@ package com.liuyaoli.myapplication
 import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.widget.ImageButton
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +16,7 @@ import androidx.media3.exoplayer.ExoPlayer
 
 class VideoStreamActivity : AppCompatActivity() {
     private lateinit var player:  ExoPlayer
+    private lateinit var gestureDetector: GestureDetector
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +26,20 @@ class VideoStreamActivity : AppCompatActivity() {
             finish()
         }
         playVideoStream()
+        gestureDetector = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener() {
+            override fun onFling(
+                e1: MotionEvent,
+                e2: MotionEvent,
+                velocityX: Float,
+                velocityY: Float
+            ): Boolean {
+                // 监听向下滑动事件，并切换到下一个视频
+                if (velocityY < -100) {
+                    playVideo("https://files.lsmcloud.top/dog_and_cat_playing.mp4")
+                }
+                return super.onFling(e1, e2, velocityX, velocityY)
+            }
+        })
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -34,12 +51,22 @@ class VideoStreamActivity : AppCompatActivity() {
         val mediaItem = MediaItem.fromUri("https://files.lsmcloud.top/xyq_playing.mp4")
         player.setMediaItem(mediaItem)
         player.prepare()
-        playerView.setOnTouchListener { _, _ ->
+        playerView.setOnTouchListener { _, event ->
+            gestureDetector.onTouchEvent(event)
+        }
+        playerView.setOnClickListener {
             playerView.useController = true
-            false
         }
         player.playWhenReady = true
 //        player.play()
+    }
+
+    private fun playVideo(uri:String){
+        player.stop()
+        player.clearMediaItems()
+        player.setMediaItem(MediaItem.fromUri(uri))
+        player.prepare()
+        player.play()
     }
 
     override fun onPause() {
